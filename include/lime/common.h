5847310c96fb0d915498de8b3f48d1ea778826c0
kuchenkruste <me@kuchenkruste.com>
2019-12-25 10:34:33 +0100

    Add OS name and architecture macros

--- include/lime/common.h
+++ include/lime/common.h
@@ -27,6 +27,26 @@ typedef int8_t  s8;
 #define OS_WINDOWS
 #endif
 
+#if _WIN64 || (__GNUC__ && (__x86_64__ || __ppc64__))
+#define OS_ARCHITECTURE_64
+#else 
+#define OS_ARCHITECTURE_32
+#endif
+
+#if __APPLE__ || __MACH__
+#define OS_NAME "Mac OS X";
+#elif _WIN32 || _WIN64
+#define OS_NAME "Windows";
+#elif __linux__
+#define OS_NAME "Linux";
+#elif __FreeBSD__
+#define OS_NAME "FreeBSD";
+#elif __unix || __unix__
+#define OS_NAME "Unix";
+#else
+#define OS_NAME "Unknown";
+#endif
+
 #if defined(OS_UNIX)
 #include <dlfcn.h>
 #elif defined(OS_WINDOWS)
